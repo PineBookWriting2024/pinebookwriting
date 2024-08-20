53c239a90aa55d19fccced8090bce6480936f418
Pine Book Writing <info@pinebookwriting.com>
2024-08-20 12:23:12 -0700

    add portfolio images

--- pages/components/BrandPortfolio.js
+++ pages/components/BrandPortfolio.js
@@ -9,27 +9,39 @@ const GLightbox = dynamic(
     { ssr: false }
 );
 const books = [
-    // { id: 1, title: 'Book One', category: 'Fiction', imageUrl: '/brand-img/fiction-1.png' },
-    { id: 1, title: 'Book Two', category: 'Fiction', imageUrl: '/brand-img/fiction-2.png' },
-    { id: 2, title: 'Book Three', category: 'Fiction', imageUrl: '/brand-img/fiction-3.png' },
-    { id: 3, title: 'Book Four', category: 'Fiction', imageUrl: '/brand-img/fiction-4.png' },
-    { id: 4, title: 'Book Five', category: 'Fiction', imageUrl: '/brand-img/fiction-5.png' },
-    { id: 5, title: 'Book Six', category: 'Fiction', imageUrl: '/brand-img/fiction-6.png' },
-    { id: 6, title: 'Book Seven', category: 'Fiction', imageUrl: '/brand-img/fiction-7.png' },
-    { id: 7, title: 'Book Eight', category: 'Fiction', imageUrl: '/brand-img/fiction-8.png' },
-    { id: 8, title: 'Book Nine', category: 'Fiction', imageUrl: '/brand-img/fiction-9.png' },
-    { id: 9, title: 'Book Ten', category: 'Fiction', imageUrl: '/brand-img/fiction-10.png' },
-    { id: 10, title: 'Book Eleven', category: 'Fiction', imageUrl: '/brand-img/fiction-11.jpg' },
-    { id: 11, title: 'Book Thirdteen', category: 'Non-Fiction', imageUrl: '/brand-img/non-fiction-1.png' },
-    { id: 12, title: 'Book Fourteen', category: 'Non-Fiction', imageUrl: '/brand-img/non-fiction-2.png' },
-    { id: 13, title: 'Book Fifteen', category: 'Non-Fiction', imageUrl: '/brand-img/non-fiction-3.png' },
-    { id: 14, title: 'Book Sixteen', category: 'Non-Fiction', imageUrl: '/brand-img/non-fiction-4.png' },
-    { id: 15, title: 'Book Seventeen', category: 'Non-Fiction', imageUrl: '/brand-img/non-fiction-5.png' },
-    { id: 16, title: 'Book Eighteen', category: 'Non-Fiction', imageUrl: '/brand-img/non-fiction-6.png' },
-    { id: 17, title: 'Book Nineteen', category: 'Non-Fiction', imageUrl: '/brand-img/non-fiction-7.png' },
-    { id: 18, title: 'Book Twenty', category: 'Non-Fiction', imageUrl: '/brand-img/non-fiction-8.png' },
-    { id: 19, title: 'Book TwentyOne', category: 'Non-Fiction', imageUrl: '/brand-img/non-fiction-9.png' },
-    { id: 20, title: 'Book TwentyTwo', category: 'Non-Fiction', imageUrl: '/brand-img/non-fiction-10.jpg' },
+    { id: 1, title: 'Book One', category: 'Fiction', imageUrl: 'brand-img/Fiction/fiction-1.png' },
+    { id: 2, title: 'Book Two', category: 'Fiction', imageUrl: 'brand-img/Fiction/fiction-2.png' },
+    { id: 3, title: 'Book Three', category: 'Fiction', imageUrl: 'brand-img/Fiction/fiction-3.png' },
+    { id: 4, title: 'Book Four', category: 'Fiction', imageUrl: 'brand-img/Fiction/fiction-4.png' },
+    { id: 5, title: 'Book Five', category: 'Fiction', imageUrl: 'brand-img/Fiction/fiction-5.png' },
+    { id: 6, title: 'Book Six', category: 'Fiction', imageUrl: 'brand-img/Fiction/fiction-6.png' },
+    { id: 7, title: 'Book Seven', category: 'Fiction', imageUrl: 'brand-img/Fiction/fiction-7.png' },
+    { id: 8, title: 'Book Eight', category: 'Fiction', imageUrl: 'brand-img/Fiction/fiction-8.png' },
+    { id: 9, title: 'Book Nine', category: 'Fiction', imageUrl: 'brand-img/Fiction/fiction-9.png' },
+    { id: 10, title: 'Book Ten', category: 'Fiction', imageUrl: 'brand-img/Fiction/fiction-10.png' },
+    { id: 11, title: 'Book Eleven', category: 'Fiction', imageUrl: 'brand-img/Fiction/fiction-11.png' },
+    { id: 12, title: 'Book twelve', category: 'Fiction', imageUrl: 'brand-img/Fiction/fiction-12.png' },
+
+
+    { id: 13, title: 'Book Thirdteen', category: 'Non-Fiction', imageUrl: '/brand-img/Non-Fiction/non-fiction-1.png' },
+    { id: 14, title: 'Book Fourteen', category: 'Non-Fiction', imageUrl: '/brand-img/Non-Fiction/non-fiction-2.png' },
+    { id: 15, title: 'Book Fifteen', category: 'Non-Fiction', imageUrl: '/brand-img/Non-Fiction/non-fiction-3.png' },
+    { id: 16, title: 'Book Sixteen', category: 'Non-Fiction', imageUrl: '/brand-img/Non-Fiction/non-fiction-4.png' },
+    { id: 17, title: 'Book Seventeen', category: 'Non-Fiction', imageUrl: '/brand-img/Non-Fiction/non-fiction-5.png' },
+    { id: 18, title: 'Book Eighteen', category: 'Non-Fiction', imageUrl: '/brand-img/Non-Fiction/non-fiction-6.png' },
+    { id: 19, title: 'Book Nineteen', category: 'Non-Fiction', imageUrl: '/brand-img/Non-Fiction/non-fiction-7.png' },
+    { id: 20, title: 'Book Twenty', category: 'Non-Fiction', imageUrl: '/brand-img/Non-Fiction/non-fiction-8.png' },
+    { id: 21, title: 'Book TwentyOne', category: 'Non-Fiction', imageUrl: '/brand-img/Non-Fiction/non-fiction-9.png' },
+    { id: 22, title: 'Book TwentyTwo', category: 'Non-Fiction', imageUrl: '/brand-img/Non-Fiction/non-fiction-10.png' },
+    { id: 23, title: 'Book TwentyThree', category: 'Non-Fiction', imageUrl: '/brand-img/Non-Fiction/non-fiction-11.png' },
+    { id: 24, title: 'Book TwentyFour', category: 'Non-Fiction', imageUrl: '/brand-img/Non-Fiction/non-fiction-12.png' },
+
+    { id: 25, title: 'Book TwentyFive', category: 'Childer-Book', imageUrl: '/brand-img/Children-Book/children-book-1.png' },
+    { id: 26, title: 'Book TwentySix', category: 'Childer-Book', imageUrl: '/brand-img/Children-Book/children-book-2.png' },
+    { id: 27, title: 'Book TwentySeven', category: 'Childer-Book', imageUrl: '/brand-img/Children-Book/children-book-3.png' },
+    { id: 28, title: 'Book TwentyEight', category: 'Childer-Book', imageUrl: '/brand-img/Children-Book/children-book-4.png' },
+    { id: 29, title: 'Book TwentyNine', category: 'Childer-Book', imageUrl: '/brand-img/Children-Book/children-book-5.png' },
+
 
 ];
 
@@ -66,7 +78,7 @@ export default function BrandPortfolio() {
                     <h2 className=" text-4xl text-black uppercase">A Collection of Stories We’ve Helped Bring to Life</h2>
                 </div>
                 <div className="flex space-x-4 justify-center mb-12">
-                    {['Fiction', 'Non-Fiction'].map(category => (
+                    {['Fiction', 'Non-Fiction', 'Childer-Book'].map(category => (
                         <button
                             key={category}
                             className={`px-4 py-2 rounded-lg ${activeCategory === category ? 'active' : 'in-active'}`}

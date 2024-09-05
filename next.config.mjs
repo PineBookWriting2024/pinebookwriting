8fcc24e226e908d0e1797888dd90cedd05625ce1
Pine Book Writing <info@pinebookwriting.com>
2024-09-05 14:37:14 -0700

    add redirection old url

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,22 @@ const nextConfig = {
   },
   async redirects() { return [ 
     { source: '/publishing-bundles', destination: '/book-publishing', permanent: true, },
-    { source: 'https://www.pinebookwriting.com/pricing', destination: 'https://www.pinebookwriting.com/packages', permanent: true, },
+    { source: '/pricing', destination: '/packages', permanent: true, },
+    { source: '/contact', destination: '/contact-us', permanent: true, },
+    { source: '/editing', destination: '/services', permanent: true, },
+    { source: '/proofreading', destination: '/services', permanent: true, },
+    { source: '/document_processing', destination: '/services', permanent: true, },
+    { source: '/book-marketing', destination: '/services', permanent: true, },
+    { source: '/graphic-designing', destination: '/services', permanent: true, },
+    { source: '/author-website', destination: '/services', permanent: true, },
+    { source: '/book-trailer', destination: '/services', permanent: true, },
+    { source: '/book-formatting', destination: '/services', permanent: true, },
+    { source: '/web-content', destination: '/services', permanent: true, },
+    { source: '/book-cover', destination: '/services', permanent: true, },
+    { source: '/book-cover', destination: '/services', permanent: true, },
+    { source: '/audio-book', destination: '/services', permanent: true, },
+    { source: '/term-and-condition', destination: '/terms-and-conditions', permanent: true, },
+    { source: '/blog', destination: '/', permanent: false, },
    ] }, 
 };
 

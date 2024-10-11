423d444df3b98b02aeadb602c2f3076d803d538a
Pine Book Writing <info@pinebookwriting.com>
2024-10-11 12:34:57 -0700

    chat add

--- pages/_app.js
+++ pages/_app.js
@@ -48,6 +48,8 @@ export default function App({ Component, pageProps }) {
         strategy="afterInteractive"
       />
       
+      <Script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=6ad75b0f-d085-4cae-9a7a-48abeb69b973"> </Script>
+
 
       <script
         dangerouslySetInnerHTML={{

f2656fdb5d7a77bb496e6a7748a21748cf4a699e
Pine Book Writing <info@pinebookwriting.com>
2024-12-11 15:18:04 -0800

    change script

--- pages/_app.js
+++ pages/_app.js
@@ -47,6 +47,9 @@ export default function App({ Component, pageProps }) {
         src="https://www.googletagmanager.com/gtag/js?id=G-9X52J8V8NK"
         strategy="afterInteractive"
       />
+
+    <Script src="https://www.googletagmanager.com/gtag/js?id=G-6NHB63Q7NV">
+    </Script>
       
       <Script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=6ad75b0f-d085-4cae-9a7a-48abeb69b973"> </Script>
 

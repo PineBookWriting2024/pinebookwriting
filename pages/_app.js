96b3f4030b5149b82bf56c0fb12fbc8560eda54a
PIne Book Publishing <167825260+PineBook2024@users.noreply.github.com>
2025-06-20 03:26:15 +0500

    rb2b code added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import React, { useEffect ,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import localFont from 'next/font/local';
 import { Poppins } from 'next/font/google';
 import Script from 'next/script';
@@ -30,18 +30,18 @@ const poppins = Poppins({
 
 export default function App({ Component, pageProps }) {
 
-   const [isModalOpen, setModalOpen] = useState(true);
-   const closeModal = () => {
+  const [isModalOpen, setModalOpen] = useState(true);
+  const closeModal = () => {
     setModalOpen(false);
-};
-    const [selectedService, setSelectedService] = useState('');
+  };
+  const [selectedService, setSelectedService] = useState('');
 
 
-    const openModal = (service) => {
-        setSelectedService(service);
-        setModalOpen(true);
-    };
-  
+  const openModal = (service) => {
+    setSelectedService(service);
+    setModalOpen(true);
+  };
+
   useEffect(() => {
     window.dataLayer = window.dataLayer || [];
     function gtag() { dataLayer.push(arguments); }
@@ -63,24 +63,29 @@ export default function App({ Component, pageProps }) {
 
   return (
     <main className={`${poppins.variable}`}>
-                <PopupProvider>
+      <PopupProvider>
 
-        <HomePopupNew/>
+        <HomePopupNew />
         {/* <PopupBundle isOpen={isModalOpen} onClose={closeModal} service={selectedService} /> */}
-          <PopupModal />
-          <Component {...pageProps} />
-        </PopupProvider>
+        <PopupModal />
+        <Component {...pageProps} />
+      </PopupProvider>
 
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-9X52J8V8NK"
         strategy="afterInteractive"
       />
 
-    <Script src="https://www.googletagmanager.com/gtag/js?id=G-362MG93QNS">
-    </Script>
-      
-      <Script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=6ad75b0f-d085-4cae-9a7a-48abeb69b973"> </Script>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-362MG93QNS">
+      </Script>
 
+      <Script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=6ad75b0f-d085-4cae-9a7a-48abeb69b973"> </Script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function () {var reb2b = window.reb2b = window.reb2b || [];if (reb2b.invoked) return;reb2b.invoked = true;reb2b.methods = ["identify", "collect"];reb2b.factory = function (method) {return function () {var args = Array.prototype.slice.call(arguments);args.unshift(method);reb2b.push(args);return reb2b;};};for (var i = 0; i < reb2b.methods.length; i++) {var key = reb2b.methods[i];reb2b[key] = reb2b.factory(key);}reb2b.load = function (key) {var script = document.createElement("script");script.type = "text/javascript";script.async = true;script.src = "https://b2bjsstore.s3.us-west-2.amazonaws.com/b/" + key + "/961Y0H4M4KNG.js.gz";var first = document.getElementsByTagName("script")[0];first.parentNode.insertBefore(script, first);};reb2b.SNIPPET_VERSION = "1.0.1";reb2b.load("961Y0H4M4KNG");}();`,
+        }}
+      />
 
       <script
         dangerouslySetInnerHTML={{
@@ -96,15 +101,15 @@ export default function App({ Component, pageProps }) {
               fbq('init', '685229296766479');
               fbq('track', 'PageView');
                           `,
-                      }}
-                    />
+        }}
+      />
 
       <noscript>
         <img height="1" width="1" style={{ display: "none" }}
           src="https://www.facebook.com/tr?id=685229296766479&ev=PageView&noscript=1"
         /></noscript>
 
-       
+
 
     </main>
   );

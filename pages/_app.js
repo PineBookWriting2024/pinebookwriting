ce5d46c93e25b295b181a4533553e30c86b1d625
PIne Book Publishing <167825260+PineBook2024@users.noreply.github.com>
2025-07-15 23:54:13 +0500

    add script

--- pages/_app.js
+++ pages/_app.js
@@ -154,7 +154,16 @@ s.parentNode.insertBefore(b, s);})(window.lintrk);`,
       </noscript>
 
 
-
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+             (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "inhutyy28l");`,
+        }}
+      />
 
 
     </main>

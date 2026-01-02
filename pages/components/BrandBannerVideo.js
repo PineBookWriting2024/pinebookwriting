6fefe79d5c1ab997ea28136c54d3d2739322be34
PIne Book Publishing <167825260+PineBook2024@users.noreply.github.com>
2026-01-03 04:19:07 +0500

    change banner

--- pages/components/BrandBannerVideo.js
+++ pages/components/BrandBannerVideo.js
@@ -2,31 +2,31 @@
 import { useEffect } from "react";
 
 export default function BrandBannerVideo({ Component = null }) {
-   useEffect(() => {
-    const snowContainer = document.getElementById("snow");
+  //  useEffect(() => {
+  //   const snowContainer = document.getElementById("snow");
 
-    const flakes = 150; // snowflakes ki density
-    const width = window.innerWidth;
-    const height = window.innerHeight;
+  //   const flakes = 150; // snowflakes ki density
+  //   const width = window.innerWidth;
+  //   const height = window.innerHeight;
 
-    for (let i = 0; i < flakes; i++) {
-      const flake = document.createElement("div");
-      flake.className = "absolute bg-white rounded-full opacity-70";
+  //   for (let i = 0; i < flakes; i++) {
+  //     const flake = document.createElement("div");
+  //     flake.className = "absolute bg-white rounded-full opacity-70";
       
-      const size = Math.random() * 4 + 2; // size 2px - 6px
-      flake.style.width = `${size}px`;
-      flake.style.height = `${size}px`;
-      flake.style.top = `${Math.random() * height}px`;
-      flake.style.left = `${Math.random() * width}px`;
-      flake.style.animation = `fall ${3 + Math.random() * 5}s linear infinite`;
+  //     const size = Math.random() * 4 + 2; // size 2px - 6px
+  //     flake.style.width = `${size}px`;
+  //     flake.style.height = `${size}px`;
+  //     flake.style.top = `${Math.random() * height}px`;
+  //     flake.style.left = `${Math.random() * width}px`;
+  //     flake.style.animation = `fall ${3 + Math.random() * 5}s linear infinite`;
       
-      snowContainer.appendChild(flake);
-    }
-  }, []);
+  //     snowContainer.appendChild(flake);
+  //   }
+  // }, []);
   
     return (
       <>
-<div id="snow" className="pointer-events-none fixed top-0 left-0 w-full h-full z-50"></div>
+{/* <div id="snow" className="pointer-events-none fixed top-0 left-0 w-full h-full z-50"></div> */}
       <section className="brand-main-banner-video flex items-start md:items-center justify-strat md:justify-center gap-4 md:py-14 h-full ">
         {/* <div className="snow"></div> */}
         <video

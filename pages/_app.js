ff5d4563e673bd9b675083d015bfdfb6541ad099
Pine Book Writing <info@pinebookwriting.com>
2026-07-24 05:09:21 +0500

    zendesk script added

--- pages/_app.js
+++ pages/_app.js
@@ -65,7 +65,7 @@ export default function App({ Component, pageProps }) {
         src="https://www.googletagmanager.com/gtag/js?id=G-9X52J8V8NK"
         strategy="afterInteractive"
       /> */}
-
+<Script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=6ad75b0f-d085-4cae-9a7a-48abeb69b973"> </Script>
       <Script src="https://www.googletagmanager.com/gtag/js?id=G-362MG93QNS" strategy="afterInteractive" />
 
       <script
@@ -112,8 +112,7 @@ window._linkedin_data_partner_ids.push(_linkedin_partner_id);`,
         }}
       />
 
-          <script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=6ad75b0f-d085-4cae-9a7a-48abeb69b973"> </script>
-
+          
       <script
         dangerouslySetInnerHTML={{
           __html: `(function(l) {
